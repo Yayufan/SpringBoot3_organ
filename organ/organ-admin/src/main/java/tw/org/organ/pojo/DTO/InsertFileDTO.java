35cedd8bef6bd7c69ad2093a586449d0b53df523
Yayufan <kamikazey2200@gmail.com>
2025-01-22 17:50:30 +0800

    Update InsertFileDTO.java

--- organ/organ-admin/src/main/java/tw/org/organ/pojo/DTO/InsertFileDTO.java
+++ organ/organ-admin/src/main/java/tw/org/organ/pojo/DTO/InsertFileDTO.java
@@ -1,7 +1,5 @@
 package tw.org.organ.pojo.DTO;
 
-import com.baomidou.mybatisplus.annotation.TableField;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
